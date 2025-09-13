6b0f44dbf85527f7cb31a046eb631780febbd623
IRGI_SEPTIANA_SURYA <irgiseptianansurya@gmail.com>
2025-09-13 10:37:43 +0700

    rolback

--- Python-Style Sticky Notes Application/App.tsx
+++
@@ -1,9 +0,0 @@
-import { StickyNote } from './components/StickyNote';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-8">
-      <StickyNote />
-    </div>
-  );
-}
